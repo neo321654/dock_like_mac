e371cb875d22e536e27511fe662e216219fc6638
neo321654 <neo321654@rambler.ru>
2024-10-31 09:30:12 +0300

    start from unidraw

--- lib/main.dart
+++ lib/main.dart
@@ -200,109 +200,104 @@ class _DockItemState<T extends Object> extends State<DockItem<T>> {
 
   @override
   Widget build(BuildContext context) {
-    return Visibility(
-      visible: isVisible, // Control visibility based on state.
-      maintainSize: true,
-      maintainAnimation: true,
-      maintainState: true,
-      child: Draggable<T>(
-        data: widget.item, // Data passed during drag and drop operations.
-        onDragStarted: () {
-          isDragging = true; // Set dragging state to true when drag starts.
-          isVisible = false; // Hide the item being dragged.
-        },
-        onDragEnd: (details) {
-          isDragging = false; // Reset dragging state when drag ends.
+    return Draggable<T>(
+      data: widget.item, // Data passed during drag and drop operations.
+      onDragStarted: () {
+        isDragging = true; // Set dragging state to true when drag starts.
+        isVisible = false; // Hide the item being dragged.
+      },
+      onDragUpdate: (dragUpdateDetails){},
+      onDragEnd: (details) {
+        isDragging = false; // Reset dragging state when drag ends.
 
-          showOverlayAnimation(
-              begin: details.offset, // Start position for overlay animation.
-              end: widget.globalOffset, // End position for overlay animation.
-              context: context);
+        showOverlayAnimation(
+            begin: details.offset, // Start position for overlay animation.
+            end: widget.globalOffset, // End position for overlay animation.
+            context: context);
 
-          resetGlobalDelta(); // Reset delta offsets after drag ends.
-        },
-        onDragCompleted: () {
-          isDragging = false; // Reset dragging state when drag completes.
-          isVisible = true; // Show the item again after completion.
-          resetGlobalDelta(); // Reset delta offsets after drag completes.
-        },
-        onDraggableCanceled: (velocity, offset) {
-          isDragging = false; // Reset dragging state if drag is canceled.
-          isVisible = true; // Show the item again if canceled.
-          resetGlobalDelta(); // Reset delta offsets after cancellation.
-        },
-        dragAnchorStrategy: (Draggable<Object> draggable, BuildContext context, Offset position) {
-          RenderBox renderObject = getRenderBoxObject(context)!; // Get render object for positioning.
+        resetGlobalDelta(); // Reset delta offsets after drag ends.
+      },
+      onDragCompleted: () {
+        isDragging = false; // Reset dragging state when drag completes.
+        isVisible = true; // Show the item again after completion.
+        resetGlobalDelta(); // Reset delta offsets after drag completes.
+      },
+      onDraggableCanceled: (velocity, offset) {
+        isDragging = false; // Reset dragging state if drag is canceled.
+        isVisible = true; // Show the item again if canceled.
+        resetGlobalDelta(); // Reset delta offsets after cancellation.
+      },
+      dragAnchorStrategy: (Draggable<Object> draggable, BuildContext context, Offset position) {
+        RenderBox renderObject = getRenderBoxObject(context)!; // Get render object for positioning.
 
-          Offset? offSet = getParentOffset(renderObject); // Get parent offset.
+        Offset? offSet = getParentOffset(renderObject); // Get parent offset.
 
-          if (offSet != null) {
-            Offset ofToGlobal = renderObject.localToGlobal(offSet) - offSet;
-            widget.setGlobalDeltaOffset(offSet); // Update global delta offset.
-            widget.setGlobalOffset(ofToGlobal); // Update global offset based on position.
+        if (offSet != null) {
+          Offset ofToGlobal = renderObject.localToGlobal(offSet) - offSet;
+          widget.setGlobalDeltaOffset(offSet); // Update global delta offset.
+          widget.setGlobalOffset(ofToGlobal); // Update global offset based on position.
+        }
+
+        return renderObject.globalToLocal(position); // Convert position to local coordinates.
+      },
+      childWhenDragging: Visibility(
+        visible:isVisible,
+        maintainSize:true,
+        maintainAnimation:true,
+        maintainState:true,
+        child :widgetFromBuilder,
+      ),
+      feedback :widgetFromBuilder,
+      child :DragTarget<T>(
+        builder:(BuildContext context,candidateData,rejectedData){
+          if (candidateData.isNotEmpty) {
+            RenderBox renderBox = context.findRenderObject() as RenderBox; // Get render box for positioning.
+
+            Offset offsetBias = getParentOffset(renderBox) ?? Offset.zero; // Calculate offset bias.
+
+            Offset ofToGlobal =
+                renderBox.localToGlobal(offsetBias) - offsetBias; // Calculate global offset.
+
+            offsetToAccept = ofToGlobal;
+
+            WidgetsBinding.instance.addPostFrameCallback((d) {
+              widget.setGlobalOffset(ofToGlobal);
+            });
+
+            offsetToLeave = offsetBias;
+
+            offsetToDelta = widget.globalDeltaOffset - offsetBias;
+
+            // Calculate horizontal or vertical shift
+            offsetToDelta = Offset(
+              renderBox.size.width * offsetToDelta.dx.sign,
+              offsetToDelta.dy,
+            );
+
+            return AnimatedOffsetWidget(
+              begin : Offset.zero,
+              end :offsetToDelta,
+              duration :const Duration(milliseconds :600),
+              child :widgetFromBuilder ,
+              builder :(context ,offset ,child){
+                return Transform.translate(
+                  offset :offset ,
+                  child :widgetFromBuilder ,
+                );
+              },
+            );
           }
 
-          return renderObject.globalToLocal(position); // Convert position to local coordinates.
+          return widgetFromBuilder; // Return default widget if no candidates are present
         },
-        childWhenDragging: Visibility(
-          visible:isVisible,
-          maintainSize:true,
-          maintainAnimation:true,
-          maintainState:true,
-          child :widgetFromBuilder,
-        ),
-        feedback :widgetFromBuilder,
-        child :DragTarget<T>(
-          builder:(BuildContext context,candidateData,rejectedData){
-            if (candidateData.isNotEmpty) {
-              RenderBox renderBox = context.findRenderObject() as RenderBox; // Get render box for positioning.
-
-              Offset offsetBias = getParentOffset(renderBox) ?? Offset.zero; // Calculate offset bias.
-
-              Offset ofToGlobal =
-                  renderBox.localToGlobal(offsetBias) - offsetBias; // Calculate global offset.
-
-              offsetToAccept = ofToGlobal;
-
-              WidgetsBinding.instance.addPostFrameCallback((d) {
-                widget.setGlobalOffset(ofToGlobal);
-              });
-
-              offsetToLeave = offsetBias;
-
-              offsetToDelta = widget.globalDeltaOffset - offsetBias;
-
-              // Calculate horizontal or vertical shift
-              offsetToDelta = Offset(
-                renderBox.size.width * offsetToDelta.dx.sign,
-                offsetToDelta.dy,
-              );
-
-              return AnimatedOffsetWidget(
-                begin : Offset.zero,
-                end :offsetToDelta,
-                duration :const Duration(milliseconds :600),
-                child :widgetFromBuilder ,
-                builder :(context ,offset ,child){
-                  return Transform.translate(
-                    offset :offset ,
-                    child :widgetFromBuilder ,
-                  );
-                },
-              );
-            }
-
-            return widgetFromBuilder; // Return default widget if no candidates are present
-          },
-          onAcceptWithDetails:(data){
-            widget.setGlobalOffset(offsetToAccept);
-            widget.onDrop(data.data ,widget.item);
-          },
-          onLeave:(data){
-            widget.setGlobalDeltaOffset(offsetToLeave);
-            widget.onDrop(data!, widget.item);
-          },
-        ),
+        onAcceptWithDetails:(data){
+          widget.setGlobalOffset(offsetToAccept);
+          widget.onDrop(data.data ,widget.item);
+        },
+        onLeave:(data){
+          widget.setGlobalDeltaOffset(offsetToLeave);
+          widget.onDrop(data!, widget.item);
+        },
       ),
     );
   }
