43996c5ab0bd7ea202aaf81a1603d61551446e78
neo321654 <neo321654@rambler.ru>
2024-11-02 23:01:58 +0300

    dr

--- lib/main2.dart
+++ lib/main2.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
-
+///
 /// [Widget] building the [MaterialApp].
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
